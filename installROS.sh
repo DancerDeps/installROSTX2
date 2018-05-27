2427c578f4a2fac221aecd036f35196f4e2a9d11
Yusu Pan <corenel@users.noreply.github.com>
2018-05-27 22:46:59 +0800

    Replace ROS soruces

--- installROS.sh
+++ installROS.sh
@@ -85,7 +85,7 @@ sudo apt-add-repository multiverse
 sudo apt-add-repository restricted
 
 # Setup sources.lst
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://mirrors.ustc.edu.cn/ros/ubuntu/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 # Setup keys
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
 # Installation
